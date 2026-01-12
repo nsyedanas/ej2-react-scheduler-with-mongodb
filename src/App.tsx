bee6023624b266a8c91634b356b373bb9b85f5d0
Syed Anas Nizam Deen <syedanas.nizamdeen@gmail.com>
2026-01-12 11:20:15 +0530

    updated version of react scheduler sample

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,18 @@ export default class App extends React.Component<{}, {}> {
 
   public render() {
     return (
+      
       <div className="control-section">
         <div className="schedule-control">
-          <ScheduleComponent id="schedule" ref={(schedule: ScheduleComponent) => this.scheduleObj = schedule} height="550px"
-            selectedDate={new Date(2017, 5, 5)} currentView="Month" eventSettings={{ dataSource: this.dataManager }}>
+          <ScheduleComponent 
+          id="schedule" 
+          ref={(schedule: ScheduleComponent | null) => { 
+              this.scheduleObj = schedule!;
+            }}
+          height="550px"
+          selectedDate={new Date(2026, 0, 1)} 
+          currentView="Month" 
+          eventSettings={ {dataSource: this.dataManager }}>
             <ViewsDirective>
               <ViewDirective option="Day" />
               <ViewDirective option="Week" />
